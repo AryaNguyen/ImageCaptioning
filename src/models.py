ed401c1daf08569eb0033b1664e3d1ae4ceca3a0
Arya Nguyen <49668914+AryaNguyen@users.noreply.github.com>
2020-03-28 15:00:08 -0400

    Finish Encoder model

--- src/models.py
+++ src/models.py
@@ -21,12 +21,24 @@ class Encoder(nn.Module):
         self.resnet = nn.Sequential(modules)
 
         # Resize image to fixed size
-
+        self.adaptive_pool = nn.AdaptiveAvgPool2d((encoded_image_size, encoded_image_size))
 
         # fine tune
+        self.fine_tune()
 
     def forward(self, images):
-        pass
+        out = self.resnet(images)
+        out = self.adaptive_pool(out)
+        out = out.permute((0, 2, 3, 1))
+        return out
+
+    def fine_tune(self, fine_tune=True):
+        for p in self.resnet.parameters():
+            p.required_grad = False
+
+        for c in list(self.resnet.children()[5:]):
+            for p in c.parameters():
+                p.required_grad = fine_tune
 
 
 class Attention(nn.Module):
